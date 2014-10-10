d5f03a751efaffc7df48db24b8c0cf0a443ff2c8
hugochan <hugochan2013@gmail.com>
2014-10-10 19:09:16 -0400

    implement send buffer
    implement sender buffer & buffer information management. sender edge is
    nearly done except timer & checksum.

--- rUDP_sender/rUDP_sender.cpp
+++ rUDP_sender/rUDP_sender.cpp
@@ -20,7 +20,13 @@ rUDP_sender::rUDP_sender(void)
 	// initialize
 	nextseqnum = 1;
 	sendbase = 1;
-	N = 50;
+	queue_buf.front = 0;
+	queue_buf.rear = 0;
+
+	// initialize Buffer Information Management
+	buf_info_manag.front = &info_node;
+	buf_info_manag.rear = &info_node;
+
 	global_msg.content = "";
 	global_msg.len = 0;
 	NewMsgFlag = false;
@@ -29,6 +35,7 @@ rUDP_sender::rUDP_sender(void)
 	// register socket
 	// every time instantiate the class, get one socket
 	sock = socket(AF_INET, SOCK_DGRAM, 0);
+
 	DWORD dwThreadId;
 	HANDLE hThread;
 	hThread = CreateThread(
@@ -59,19 +66,20 @@ int rUDP_sender::bindsocket(sockaddr* addr, int size)
 	return 0;
 }
 
-// send function, multi-thread not allowed, because of using global flag
+// send function, blocking & multi-thread not allowed, because of using global flag
 int rUDP_sender::sendMsg(char* message, int msg_len)
 {
 	int ret;
 	NewMsgFlag = true; // set flag
+	message[msg_len] = 0;
 	global_msg.content = message;
 	global_msg.len = msg_len;
 	while (refuseFlag == 0);
-	if (refuseFlag == 1)
+	if (refuseFlag > 0)
 	{
-		ret = 0;
+		ret = refuseFlag;
 	}
-	else if (refuseFlag == 2)
+	else if (refuseFlag == -1)
 	{
 
 		ret = -1;
@@ -91,13 +99,14 @@ int rUDP_sender::registerRemoteAddr(sockaddr* r_addr, int addr_size)
 	return 0;
 }
 
-int rUDP_sender::make_seg(Segment* seg, int nextseqnum, int nextacknum, char* data, int checksum)
+int rUDP_sender::make_seg(Segment* seg, int nextseqnum, int nextacknum, char* data, int dlen, int checksum)
 {
 	seg->seqnum = nextseqnum;
 	seg->acknum = nextacknum;
-	seg->length = strlen(data);// length of data
+	seg->length = dlen;// length of data
 	seg->checksum = checksum;
-	seg->payload = data;
+	memcpy(seg->payload, data, dlen);
+	seg->payload[dlen] = 0;
 	return 0;
 }
 
@@ -131,6 +140,7 @@ DWORD WINAPI rUDP_sender::sender_proc(rUDP_sender* class_ptr)
 {
 	SOCKET s = class_ptr->sock;
 	bool timeout = false;
+	
 	// select
 	int retval, len;
 	fd_set readfds;
@@ -146,9 +156,13 @@ DWORD WINAPI rUDP_sender::sender_proc(rUDP_sender* class_ptr)
 	cout << "select error !" << endl;
 
 
-	int check_sum;
-	char recvbuf[1024];
+	int check_sum, i, j, index;
 	Segment* recv_seg = new Segment;
+	Segment seg;
+	char resend_buf[1024];
+
+	Info buf_info;
+
 
 	while (1)
 	{
@@ -156,24 +170,35 @@ DWORD WINAPI rUDP_sender::sender_proc(rUDP_sender* class_ptr)
 		if (NewMsgFlag == true)
 		{
 			NewMsgFlag = false;
-			if (nextseqnum < sendbase + N)
+			if (nextseqnum < sendbase + CWND)
 			{
-				Segment seg;
-				
-				// get checksum
-				check_sum = class_ptr->getchecksum(nextseqnum, 0, global_msg.content, global_msg.len);
+				// send the new message once allowed, no waiting
 
-				// send buffer to be done £¡£¡£¡
-				global_msg.content[global_msg.len] = 0;// add terminator
-				class_ptr->make_seg(&seg, nextseqnum, 0, global_msg.content, check_sum);// make segment
+				// copy to send buffer (enqueue)
+				for (i = 0; i < global_msg.len; i++)
+				{
+					if (class_ptr->enqueue(&queue_buf, global_msg.content[i]) == -1)
+						break;
+				}
+				class_ptr->refuse_data(i); // response to upper layer
+
+				// mantain Buffer Information Management (enlqueue)
+				buf_info.seqnum = nextseqnum;
+				buf_info.length = i;
+				class_ptr->enlqueue(&buf_info_manag, buf_info);
+
+				// get checksum
+				check_sum = class_ptr->getchecksum(nextseqnum, 0, global_msg.content, i);
+				
+				// make & send segment
+				class_ptr->make_seg(&seg, nextseqnum, 0, global_msg.content, i, check_sum);// make segment
 				class_ptr->sendSeg(seg);// send segment
 				if (sendbase == nextseqnum)
 					class_ptr->reset_timer();
-				nextseqnum += seg.length;
-				refuse_data(false);// accept
+				nextseqnum += i;
 			}
 			else
-				refuse_data(true);// refuse
+				refuse_data(0); // response to upper layer
 		}
 
 		// select
@@ -196,6 +221,21 @@ DWORD WINAPI rUDP_sender::sender_proc(rUDP_sender* class_ptr)
 					if (sendbase < nextseqnum)
 						class_ptr->reset_timer();
 
+					// mantain Buffer Information Management (delqueue)
+					index = class_ptr->lookuplqueue(&buf_info_manag, recv_seg->acknum);
+					for (j = 0; j < index + 1; j++)
+					{
+						if (class_ptr->delqueue(&buf_info_manag, &buf_info) == -1)
+						{
+							cout << "delqueue error!" << endl;
+							system("pause");
+							exit(0);
+						}
+					}
+
+					// free send buffer (dequeue)
+					queue_buf.front = (recv_seg->acknum - 1) % MAXNUM;
+
 				}
 				else // corrupt
 				{
@@ -206,29 +246,58 @@ DWORD WINAPI rUDP_sender::sender_proc(rUDP_sender* class_ptr)
 			{
 				// do nothing
 			}
-
-
 		}
 		
 		// timeout event
 		if (timeout == true)
 		{
+			if (class_ptr->checklqueue(&buf_info_manag, &buf_info) == -1)
+			{
+				cout << "buf_info_manag  error!"<< endl;
+				system("pause");
+				exit(0);
+			}
+			if (buf_info.seqnum == sendbase)
+			{
+				if ((sendbase % MAXNUM) + buf_info.length - 1 < MAXNUM)
+				{
+					memcpy(resend_buf, queue_buf.buf + (sendbase % MAXNUM), buf_info.length);
+				}
+				else
+				{
+					memcpy(resend_buf, queue_buf.buf + (sendbase % MAXNUM), MAXNUM - (sendbase % MAXNUM));
+					memcpy(resend_buf, queue_buf.buf, buf_info.length - (MAXNUM - (sendbase % MAXNUM)));
+				}
+				
+				// get checksum
+				check_sum = class_ptr->getchecksum(sendbase, 0, resend_buf, buf_info.length);
 
+				// make & send segment
+				class_ptr->make_seg(&seg, sendbase, 0, resend_buf, buf_info.length, check_sum);// make segment
+				class_ptr->sendSeg(seg);// send segment
 
+				class_ptr->reset_timer();
+			}
+			else
+			{
+				cout << "buf_info_manag error!" << endl;
+				system("pause");
+				exit(0);
+			}
 		}
-		
-
-
-
 	}
 }
 
-int rUDP_sender::refuse_data(bool flag)
+int rUDP_sender::refuse_data(int flag)
 {
-	if (flag == true)
-		refuseFlag = 2;
-	else
-		refuseFlag = 1;
+	if (flag == 0) // refuse
+		refuseFlag = -1;
+	else if (flag > 0) // num of sent
+	{
+		refuseFlag = flag;
+	}
+	else // treated as refuse
+		refuseFlag = -1;
 	return 0;
 }
 
@@ -246,6 +315,115 @@ int rUDP_sender::getchecksum(int seqnum, int acknum, char* payload, int length)
 {
 	// to be done
 	return 0;
+}
+
+
+// enqueue for BufInfoManagLq
+int rUDP_sender::enlqueue(BufInfoManagLq *q, Info x)
+{
+	InfoNodeType  *s = new InfoNodeType;
+	if (s != NULL)
+	{
+		s->info = x;
+		s->next = NULL;	 // generate new node
+		q->rear->next = s;	// add new node in a queue
+		q->rear = s;		 // update rear pointer
+		return 0;
+	} 
+	else 
+		return -1; 
+}
+
+// dequeue for BufInfoManagLq
+int rUDP_sender::delqueue(BufInfoManagLq *q, Info* x)
+{
+	InfoNodeType  *p;
+	if (q->front->next == NULL)
+		return -1;
+	else
+	{
+		p = q->front->next;   // get front node
+		*x = p->info;			// get element
+		q->front->next = p->next; // update front pointer
+		if (p->next == NULL)    // if length of queue equals to one
+			q->rear = q->front; // update rear pointer
+		delete p;		       // free the node
+		return 0;
+	}
+}
+
+// check for BufInfoManagLq
+int rUDP_sender::checklqueue(BufInfoManagLq* q, Info* x)
+{
+	InfoNodeType  *p;
+	if (q->front->next == NULL)
+		return -1;
+	else
+	{
+		p = q->front->next;   // get front node
+		*x = p->info;			// get element
+		return 0;
+	}
+}
+
+
+// // look up for the specific element, return index
+int rUDP_sender::lookuplqueue(BufInfoManagLq* q, int cond_seqnum) // look up for the specific element
+{
+	int index = -1;
+	InfoNodeType* x = q->front->next;
+	while (x != NULL)
+	{
+		if (x->info.seqnum + x->info.length - 1 >= cond_seqnum)  
+		{
+			// in this design, must have x->info.seqnum + x->info.length = cond_seqnum
+			break;
+		}
+		index += 1;
+		x = x->next;
+	}
+	return index;
+}
+
+// enqueue for Sender Buffer
+int rUDP_sender::enqueue(QueueBuf* q, char x)
+{
+	if ((q->rear + 1) % MAXNUM == q->front) // full
+		return -1;
+	else
+	{ 
+		q->rear = (q->rear + 1) % MAXNUM; // update rear
+		q->buf[q->rear] = x;	   // add new element
+		return 0; 
+	}
+}
+
+// dequeue Sender Buffer
+int rUDP_sender::dequeue(QueueBuf *q, char* x)
+{
+	if (q->rear == q->front) // empty queue
+	{	 
+		return -1;
+	}
+	else
+	{
+		q->front = (q->front + 1) % MAXNUM; // update rear
+		*x = q->buf[q->front];
+		return 0;
+	}
+}
+
+int rUDP_sender::checkqueue(QueueBuf* q, char* x)
+{
+	if (q->rear == q->front) // empty queue
+	{
+		return -1;
+	}
+	else
+	{
+		*x = q->buf[(q->front + 1) % MAXNUM];
+		return 0;
+	}
 }
 
 DWORD __stdcall startMethodInThread(LPVOID arg)
--- rUDP_sender/rUDP_sender.h
+++ rUDP_sender/rUDP_sender.h
@@ -23,31 +23,75 @@ private:
 		int acknum;
 		int length;
 		int checksum;
-		char* payload;// alter
+		char payload[128];
 	}Segment;
 
-	int nextseqnum, sendbase, N;
+	int nextseqnum, sendbase;
+	#define CWND (int)50
 	struct msg
 	{
 		char* content;
 		int len;
 	}global_msg;
 
+	// remote addr
 	struct remoteAddr
 	{
 		sockaddr remote_addr;
 		int size;
 	}remote;
+	
+	// Sender Buffer
+	// cirular queue
+	#define MAXNUM CWND + 1
+	typedef struct
+	{
+		char buf[MAXNUM];
+		int front, rear;
+	}QueueBuf;
+	QueueBuf queue_buf;
+
+	// Buffer Information Management 
+	// linked queue
+	typedef struct // element type
+	{
+		int seqnum = 0;
+		int length = 0;
+	}Info;
+
+	typedef struct node
+	{ 
+		Info info;
+		node* next = NULL;
+	}InfoNodeType;
+	InfoNodeType info_node;
+
+	typedef struct
+	{
+		InfoNodeType* front;
+		InfoNodeType* rear;
+	}BufInfoManagLq;
+	BufInfoManagLq buf_info_manag;
+
+
 	bool NewMsgFlag;
-	int refuseFlag;// 0 - default, 1 - accept, 2 - refuse
+	int refuseFlag;// 0 - default, positive num(num of sent) - accept, -1 - refuse
 
 
 	// Function
-	int make_seg(Segment* seg, int nextseqnum, int nextacknum, char* data, int checksum);
+	int make_seg(Segment* seg, int nextseqnum, int nextacknum, char* data, int dlen, int checksum);
 	int sendSeg(Segment seg);
 	int recvSeg(Segment* seg);
 	int reset_timer(void);
 	int getchecksum(int seqnum, int acknum, char* payload, int length);
-	int refuse_data(bool flag);
+	int refuse_data(int flag);
+	int enlqueue(BufInfoManagLq* q, Info x);
+	int delqueue(BufInfoManagLq* q, Info* x);
+	int checklqueue(BufInfoManagLq* q, Info* x); // check first element
+	int lookuplqueue(BufInfoManagLq* q, int cond_seqnum); // look up for the specific element
+	int enqueue(QueueBuf* q, char x);
+	int dequeue(QueueBuf* q, char* x);
+	int checkqueue(QueueBuf* q, char* x); // check first element
+
 };
 
